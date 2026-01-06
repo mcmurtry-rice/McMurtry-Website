2ee9ab676e2d857e85dd498c740119bb40214ea7
Antony Saleh <antony.saleh2017@gmail.com>
2026-01-06 22:42:39 +0200

    Fix CSS and images for custom domain
    Removed the subdirectory basePath configuration and reverted image paths
    so the site will work properly when deployed to mcmurtry.rice.edu

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 // next.config.js
 const withCSS = require('@zeit/next-css')
 
-const basePath = '/McMurtry-Website'
-
 module.exports = withCSS({
-  // Use basePath for GitHub Pages deployment at subdirectory
-  basePath: basePath,
-  assetPrefix: basePath,
-  env: {
-    BASE_PATH: basePath,
-  },
-  publicRuntimeConfig: {
-    basePath: basePath,
-  },
+  // No basePath needed for custom domain deployment
 })
