0779098d898ab14212a6e1f603e3b84ff1865482
Clin Shaiju <63211630+ClinShaiju@users.noreply.github.com>
2026-08-31 01:44:33 -0500

    deploy: SMR phone link, description, and portrait

---
+++ docs/_next/static/webpack/styles.51c2fd0697f5b30ee085.hot-update.js
@@ -0,0 +1,22 @@
+webpackHotUpdate_N_E("styles",{
+
+/***/ "./components/Footer/Footer.css":
+false,
+
+/***/ "./components/navbar/Navbar.css":
+false,
+
+/***/ "./pages/people/SMR/index.css":
+false,
+
+/***/ 2:
+false,
+
+/***/ 3:
+false,
+
+/***/ 4:
+false
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzdGF0aWMvd2VicGFjay9zdHlsZXMuNTFjMmZkMDY5N2Y1YjMwZWUwODUuaG90LXVwZGF0ZS5qcyIsInNvdXJjZVJvb3QiOiIifQ==
